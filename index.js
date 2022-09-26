2b6630d003d80eae803e3783cbb64b29e81aca0e
Marquell <starjet164@gmail.com>
2022-09-26 13:34:41 -0500

    Final Poduct

--- index.js
+++ index.js
@@ -3,23 +3,20 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 
- let license = "MIT"
-
- function generateBadgeForLicense() {
+function generateBadgeForLicense(license) {
     if (license === "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]";
-     }
-
-    else if (license === "MPL 2.0"){
-    return  "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-     }
-
-    else if(license === "ODbL"){
-    return "[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)"
-    }  
-    else (license === "Zlib")
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]";
+    }
+   else if (license === "MPL 2.0") {
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    }
+    else if (license === "ODbL") {
+        return "[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)"
+    }
+    else {
     return "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)"
- }
+    }
+}
 
 inquirer
     .prompt([
@@ -46,15 +43,26 @@ inquirer
             choices: ["HTML", "CSS", "Javascript", "python"]
         },
         {
-            type: 'checkbox',
+            type: 'list',
             message: 'what Licenses are used',
             name: 'Badges',
             choices: ["MIT", "MPL 2.0", "Zlib", "ODbL"]
-        }, 
+        },
         {
             type: 'input',
             message: 'How Is This Program Used',
             name: 'Usage',
+        },
+        {
+            type: 'Test',
+            message: 'Librarrys and test needed and taken',
+            name: 'test',
+        },
+        {
+            type: 'checkbox',
+            message: 'do you want contributions',
+            name: 'cont',
+            choices: ["yes","No"]
         },
         {
             type: 'input',
@@ -64,30 +72,36 @@ inquirer
     ])
 
     .then((response) => {
-let readMeTemplate = ` # ${response.readMeName}
+        console.log(response)
+        let readMeTemplate = `# ${response.readMeName}
 ---
 ## Table of Contents
-* Introduction
-* Usage
-* Installation
-* Licenses
-* Tests
-* Questions
-* Contributing
+* [Introduction](#Introduction)
+* [Instructions](#Instructions)
+* [Installation](#Installation)
+* [Licensing](#Licensing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+* [Contributing](#Contributing)
 ---
 ## Introduction
-###Project Created By ${response.Name}
-###${response.Intro}
-###${response.instillationInput} Was used to create this program
+### Project Created By ${response.Name}
+### ${response.Intro}
+### ${response.instillationInput} Was used to create this program
 ---
-##Licensing Involved
-${generateBadgeForLicense()}
+## Licensing Involved
+${generateBadgeForLicense(response.Badges)}
 ---
-## Instructions On How To Use
-###${response.Usage}
-
+## Instructions On How To Use 
+### ${response.Usage}
+---
+## Tests
+### ${response.test}
+---
+## Contributing
+### ${response.cont} contributions at https://github.com/MarMar164
 `
-        fs.writeFile('response.md', readMeTemplate, (err) =>
-            err ? console.error(err) : console.log('Success!')
-        )
-    })
+fs.writeFile('response.md', readMeTemplate, (err) =>
+err ? console.error(err) : console.log('Success!')
+)
+})
--- response.md
+++ response.md
@@ -1,28 +1,27 @@
- # as
-
+# ReadMe Gen
 ---
 ## Table of Contents
-* Introduction
-* Usage
-* Installation
-* Licenses
-* Tests
-* Questions
-* Contributing
+* [Introduction](#Introduction)
+* [Instructions](#Instructions)
+* [Installation](#Installation)
+* [Licensing](#Licensing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+* [Contributing](#Contributing)
 ---
 ## Introduction
-###Project Created By da
-###sd
-###d Was used to create this program
+### Project Created By Marquell Woodson
+### This is a read me gen that will streamline the process of creating readMe's and incrase there quality I used it to create the read me you are seeing 
+### node VScode inquirer 8.2.4 Was used to create this program
 ---
-## Instructions On How To Use
-###a
-
-
-
-
-
-
-
-
-
+## Licensing Involved
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)]
+---
+## Instructions On How To Use 
+### You use this program by ansering the prompts that are givien in quality then allowing the program to create your readme 
+---
+## Tests
+### Node is needed to use this and the onley test were repeeted useage till the program worked
+---
+## Contributing
+### yes contributions at https://github.com/MarMar164
